d9e1c27bd2f4222b924008a2a3fa13b532899c05
simon-mo <xmo@berkeley.edu>
2018-07-26 02:55:16 -0700

    Fix run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-HOST_ADDR=`python -c "import socket;print(socket.getfqdn())"`
-docker run -d --runtime=nvidia -p 9999:9999 -e HOST_ADDR=$(HOST_ADDR) --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock --label ai.scalabel.app=orchestrator simonmok/scalabel-orch
+docker run -d --runtime=nvidia -p 9999:9999 -e HOST_ADDR=`python -c "import socket;print(socket.getfqdn())"` --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock --label ai.scalabel.app=orchestrator simonmok/scalabel-orch
