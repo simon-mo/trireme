46736c2d72f822fd10f246edcf6acb2046179367
simon-mo <xmo@berkeley.edu>
2018-07-26 01:38:39 -0700

    use conda

--- run.sh
+++ run.sh
@@ -1,7 +1,1 @@
-docker run \
-    -d \ #daemon mode
-    --runtime=nvidia \ # to check gpu stat
-    -p 9999:9999 \
-    --restart on-failure \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    simonmok/scalabel-orch
+docker run -d --runtime=nvidia -p 9999:9999 --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock simonmok/scalabel-orch
