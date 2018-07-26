3e056777ffdae042e8387df9301bf6746fcb0c71
Simon Mo <xmo@berkeley.edu>
2018-07-26 02:50:25 -0700

    Move host addr to runtime arg

--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
 image:
-	HOST_ADDR=`python -c "import socket;print(socket.getfqdn())"`
-	docker build -t simonmok/scalabel-orch -f dockerfiles/OrchestratorDockerfile --build-arg HOST_ADDR=$(HOST_ADDR) .
+	docker build -t simonmok/scalabel-orch -f dockerfiles/OrchestratorDockerfile .
 	docker build -t simonmok/scalabel-mnist -f dockerfiles/MNISTDockerfile  .
 deploy:
 	bash run.sh
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-docker run -d --runtime=nvidia -p 9999:9999 --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock --label ai.scalabel.app=orchestrator simonmok/scalabel-orch
+HOST_ADDR=`python -c "import socket;print(socket.getfqdn())"`
+docker run -d --runtime=nvidia -p 9999:9999 -e HOST_ADDR=$(HOST_ADDR) --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock --label ai.scalabel.app=orchestrator simonmok/scalabel-orch
